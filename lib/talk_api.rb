c3f097ffd0df12bccffc2eb3ab1f26c536e7a082
Ryo Nakano <ryo.z.nakano@gmail.com>
2020-08-22 22:48:04 +0900

    Add HTTPError class

--- lib/talk_api.rb
+++ lib/talk_api.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require 'talk_api/version'
 require 'talk_api/client'
+require 'talk_api/error'
 require 'talk_api/request'
 require 'talk_api/response'
 require 'talk_api/result'
+require 'talk_api/version'
---
+++ lib/talk_api/error.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module TalkApi
+  # Error class
+  class HTTPError < RuntimeError
+  end
+end
