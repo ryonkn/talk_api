b63016ee3856e582fd706c2a542e776665cac48d
Ryo Nakano <ryo.z.nakano@gmail.com>
2020-08-24 22:46:32 +0900

    Fix to set correct User-Agent

--- lib/talk_api/request.rb
+++ lib/talk_api/request.rb
@@ -7,15 +7,13 @@ module TalkApi
   # API request class
   class Request
     ENDPOINT_URL = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
-    HEADERS = [
-      ['User-Agent', "#{self}/#{VERSION}"]
-    ].freeze
+    HEADERS = { 'User-Agent' => "#{self}/#{VERSION}" }.freeze
 
     def call(api_key, message)
       query = message.byteslice(0, 2048).scrub('')
       begin
-        client = HTTPClient.new
-        response = client.post(ENDPOINT_URL, { apikey: api_key, query: query }, header: HEADERS)
+        client = HTTPClient.new(default_header: HEADERS)
+        response = client.post(ENDPOINT_URL, { apikey: api_key, query: query })
       rescue StandardError => e
         raise HTTPError, e.message
       end
